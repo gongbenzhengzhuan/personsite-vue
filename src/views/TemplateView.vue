98b4be4268aa6b22b8b4a209a8b6abdab9101ec4
gongbenzheng <1459490255@qq.com>
2023-04-24 13:08:49 +0800

    vue-个人技能展示vue前端

---
+++ src/views/TemplateView.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>{{ message }}</h1>
+    <div>
+        <input type="checkbox" id="todo-item" checked="false" />
+        <label for="todo-item">My Todo Item</label>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      message: 'Hello World!,我的从js传入的值'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
